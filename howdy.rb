5de466afd3a3c13fc59b9042919ed8ab1b3eb5c7
Meseret Yohannes <109820673+MYohannes3@users.noreply.github.com>
2023-08-02 21:02:13 +0000

    initial commit

---
+++ howdy.rb
@@ -0,0 +1,21 @@
+require "active_support/all"
+require "./goodbye.rb"
+
+# my_string = "Howdy there!"
+# pp my_string
+
+pp 1.ordinalize    # => "1st"
+pp 2.ordinalize    # => "2nd"
+pp 53.ordinalize   # => "53rd"
+pp 2009.ordinalize # => "2009th"
+pp -21.ordinalize  # => "-21st"
+pp -134.ordinalize # => "-134th"
+
+
+pp "table".pluralize     # => "tables"
+pp "ruby".pluralize      # => "rubies"
+pp "equipment".pluralize # => "equipment"
+
+pp "tables".singularize    # => "table"
+pp "rubies".singularize    # => "ruby"
+pp "equipment".singularize # => "equipment"
